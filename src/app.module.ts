4c792ed75bd45bba1eec3de7b4defb5759e2214b
Tran Van Hai <haitv@danhkhoi.com.vn>
2022-09-26 10:36:30 +0700

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { TodoService } from './services/todo.service';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://vanhai240x:2Adgjmptw @cluster0.glihtj4.mongodb.net/?retryWrites=true&w=majority',
+      'mongodb+srv://vanhai240x:2Adgjmptw@cluster0.glihtj4.mongodb.net/?retryWrites=true&w=majority',
     ),
     MongooseModule.forFeature([
       { name: Todo.name, schema: TodoSchema },
@@ -19,3 +19,4 @@ import { TodoService } from './services/todo.service';
   providers: [TodoService],
 })
 export class AppModule {}
+  
